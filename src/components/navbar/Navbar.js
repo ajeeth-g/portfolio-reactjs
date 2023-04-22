7bcd745a85edecb8e376011b4bf9222df1e9f47e
ajeeth-g <ajeetheswari14@gmail.com>
2023-04-23 01:21:04 +0530

    code optimized in about section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,67 +3,116 @@ import { Link, NavLink } from "react-router-dom";
 import { RxChevronDown } from "react-icons/rx";
 import { BsSend } from "react-icons/bs";
 import { FiCopy } from "react-icons/fi";
+import { handleCopyEmail } from "../../data";
 import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
 import "./navbar.css";
 
 const Navbar = ({ mode, setMode }) => {
   const [panel, setPanel] = useState(false);
 
-  // DARK MODE
+  // DARK MODE CODE START
   const handleDarkMode = () => {
     setMode((prevTheme) => {
       setMode(!mode);
     });
   };
-
+  
   useEffect(() => {
     document.body.style.setProperty("--bg-main", mode ? "#212224" : "#f6f7f2");
     document.body.style.setProperty("--text-main", mode ? "#fff" : "#23282e");
+    document.body.style.setProperty("--text-muted", mode ? "#fff" : "#4f5358");
+    document.body.style.setProperty(
+      "--bg-main-300",
+      mode ? "#2e2e31" : "#ebebe8"
+    );
+    document.body.style.setProperty("--bg-panel", mode ? "#2e2e31" : "#e6e6dd");
+    document.body.style.setProperty(
+      "--para-secondary",
+      mode ? "rgba(255, 255, 255, 0.6)" : "#4f5358"
+    );
+    document.body.style.setProperty(
+      "--bg-main-300",
+      mode ? "#2e2e31" : "#ebebe8"
+    );
   }, [mode]);
 
+  // DARK MODE CODE END
+
   return (
-    <nav className={`container nav__container`}>
+    <nav className="container nav__container">
       <div className="nav__left">
         <Link to="/" className="logo">
           Ajeeth
         </Link>
       </div>
       <div className="nav__right">
-        <div className="nav__menu">
+        <div className="nav__right-menu">
           <ul className="nav__links">
             <li>
               <NavLink to="/">About</NavLink>
+            </li>
+            <li>
               <NavLink to="/work">
                 <div className="work__notification">
                   Work <span>4</span>
                 </div>
               </NavLink>
+            </li>
+            <li>
               <NavLink to="/contact">Contact</NavLink>
             </li>
           </ul>
-          <div className="nav__info">
-            <RxChevronDown
-              onClick={() => setPanel(!panel)}
-              className="btn-downArrow"
-            />
-            <div className={`nav__panel ${panel ? "" : "panelOpen"}`}>
-              <div className="nav__panel-1">
-                <ul className="nav__info--details">
-                  <li>
+
+          <div className="nav__downArrow-info">
+            <button>
+              <RxChevronDown
+                onClick={() => setPanel(!panel)}
+                className="btn-downArrow"
+              />
+            </button>
+            <div
+              className={`nav__downArrow-panel ${panel ? "" : "panelClose"}`}
+            >
+              <div className="nav__list">
+                <ul className="nav__lists">
+                  <li className="list__item">
+                    <button>
+                      <BsSend />
+                    </button>
                     <a href="mailto:ajeetheswari14@gmail.com">
-                      <BsSend className="icon" /> Send me a message
+                      Send me a message
                     </a>
                   </li>
-                  <li>
-                    <FiCopy className="icon" /> Copy my email address
+                  <li onClick={handleCopyEmail} className="list__item">
+                    <button>
+                      <FiCopy />{" "}
+                    </button>{" "}
+                    Copy my email address
                   </li>
                 </ul>
               </div>
-              <div className="nav__panel-2">
-                <Link to="https://github.com/ajeeth-g">Github</Link>
-                <Link to="https://www.instagram.com/">Instagram</Link>
-                <Link to="https://www.facebook.com/">facebook</Link>
-                <Link to="https://www.naukri.com/mnjuser/homepage">Naukri</Link>
+              <div className="contact-list">
+                <Link to="https://github.com/ajeeth-g" target="_blank">
+                  Github
+                </Link>
+                <Link
+                  to="https://www.instagram.com/alone_like_men/"
+                  target="_blank"
+                >
+                  Instagram
+                </Link>
+                <Link
+                  to="https://www.facebook.com/profile.php?id=100018785507075"
+                  target="_blank"
+                >
+                  facebook
+                </Link>
+                <Link
+                  to="https://www.naukri.com/mnjuser/homepage"
+                  target="_blank"
+                >
+                  Naukri
+                </Link>
               </div>
             </div>
           </div>
