15af88069de1daa1aead4e3b8f890554cb4db01e
ajeeth-g <ajeetheswari14@gmail.com>
2023-04-25 19:43:50 +0530

    about page

--- src/pages/work/aboutPage/AboutPage.js
+++ src/pages/work/aboutPage/AboutPage.js
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
 const AboutPage = () => {
   return (
-    <section className="container container__body aboutPage__container">
+    <section className="container aboutPage__container">
       <div className="container__left">About</div>
       <h2 className="aboutPage">
-        <a href="/">Learn more about me</a>
+        <Link to="/" >Learn more about me</Link>
       </h2>
     </section>
   );
