22cd49b8036a7c7358df2a4441c9c860e643219c
ajeeth-g <ajeetheswari14@gmail.com>
2023-06-10 12:09:36 +0530

    btn

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { RxChevronDown } from "react-icons/rx";
-import { BsSend } from "react-icons/bs";
-import { FiCopy } from "react-icons/fi";
-import { handleCopyEmail } from "../../data";
-import { projects } from "../../pages/work/projects/data";
-import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
-import "./navbar.css";
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { RxChevronDown } from 'react-icons/rx';
+import { BsSend } from 'react-icons/bs';
+import { FiCopy } from 'react-icons/fi';
+import { handleCopyEmail } from '../../data';
+import { projects } from '../../pages/work/projects/data';
+import { MdDarkMode, MdOutlineDarkMode } from 'react-icons/md';
+import './navbar.css';
 
 const Navbar = ({ mode, setMode }) => {
   const work = projects.length;
@@ -21,32 +21,32 @@ const Navbar = ({ mode, setMode }) => {
   };
 
   useEffect(() => {
-    document.body.style.setProperty("--bg-main", mode ? "#212224" : "#f6f7f2");
-    document.body.style.setProperty("--text-main", mode ? "#fff" : "#23282e");
-    document.body.style.setProperty("--text-muted", mode ? "#fff" : "#4f5358");
-    document.body.style.setProperty("--text-body", mode ? "#fff" : "#23282e");
+    document.body.style.setProperty('--bg-main', mode ? '#212224' : '#f6f7f2');
+    document.body.style.setProperty('--text-main', mode ? '#fff' : '#23282e');
+    document.body.style.setProperty('--text-muted', mode ? '#fff' : '#4f5358');
+    document.body.style.setProperty('--text-body', mode ? '#fff' : '#23282e');
     document.body.style.setProperty(
-      "--card-shadow",
-      mode ? "0 16px 40px 12px #171717" : "0 16px 40px 12px #cdcdc4"
+      '--card-shadow',
+      mode ? '0 16px 40px 12px #171717' : '0 16px 40px 12px #cdcdc4'
     );
     document.body.style.setProperty(
-      "--bg-main-300",
-      mode ? "#2e2e31" : "#ebebe8"
+      '--bg-main-300',
+      mode ? '#2e2e31' : '#ebebe8'
     );
-    document.body.style.setProperty("--bg-panel", mode ? "#2e2e31" : "#e6e6dd");
+    document.body.style.setProperty('--bg-panel', mode ? '#2e2e31' : '#e6e6dd');
     document.body.style.setProperty(
-      "--para-secondary",
-      mode ? "rgba(255, 255, 255, 0.6)" : "#4f5358"
+      '--para-secondary',
+      mode ? 'rgba(255, 255, 255, 0.6)' : '#4f5358'
     );
     document.body.style.setProperty(
-      "--bg-main-300",
-      mode ? "#2e2e31" : "#ebebe8"
+      '--bg-main-300',
+      mode ? '#2e2e31' : '#ebebe8'
     );
 
     const handleClickOutside = (event) => {
       if (
         panel &&
-        !event.target.closest(".nav__downArrow-info") &&
+        !event.target.closest('.nav__downArrow-info') &&
         downArrowRef.current &&
         !downArrowRef.current.contains(event.target)
       ) {
@@ -54,87 +54,87 @@ const Navbar = ({ mode, setMode }) => {
       }
     };
 
-    document.addEventListener("click", handleClickOutside);
+    document.addEventListener('click', handleClickOutside);
 
     return () => {
-      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener('click', handleClickOutside);
     };
   }, [mode, panel]);
 
   // DARK MODE CODE END
 
   return (
-    <nav className="container nav__container">
-      <div className="nav__left">
-        <Link to="/" className="logo">
+    <nav className='container nav__container'>
+      <div className='nav__left'>
+        <Link to='/' className='logo'>
           Ajeeth
         </Link>
       </div>
-      <div className="nav__right">
-        <div className="nav__right-menu">
-          <ul className="nav__links">
+      <div className='nav__right'>
+        <div className='nav__right-menu'>
+          <ul className='nav__links'>
             <li>
-              <NavLink to="/">About</NavLink>
+              <NavLink to='/'>About</NavLink>
             </li>
             <li>
-              <NavLink to="/work">
-                <div className="work__notification">
+              <NavLink to='/work'>
+                <div className='work__notification'>
                   Work <span>{work}</span>
                 </div>
               </NavLink>
             </li>
             <li>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink to='/contact'>Contact</NavLink>
             </li>
           </ul>
 
-          <div className="nav__downArrow-info" ref={downArrowRef}>
+          <div className='nav__downArrow-info' ref={downArrowRef}>
             <button>
               <RxChevronDown
                 onClick={() => setPanel(!panel)}
-                className="btn-downArrow"
+                className='btn-downArrow'
               />
             </button>
             <div
-              className={`nav__downArrow-panel ${panel ? "" : "panelClose"}`}
+              className={`nav__downArrow-panel ${panel ? '' : 'panelClose'}`}
             >
-              <div className="nav__list">
-                <ul className="nav__lists">
-                  <li className="list__item">
+              <div className='nav__list'>
+                <ul className='nav__lists'>
+                  <li className='list__item'>
                     <button>
                       <BsSend />
                     </button>
-                    <a href="mailto:ajeetheswari14@gmail.com">
+                    <a href='mailto:ajeetheswari14@gmail.com'>
                       Send me a message
                     </a>
                   </li>
-                  <li onClick={handleCopyEmail} className="list__item">
+                  <li onClick={handleCopyEmail} className='list__item'>
                     <button>
-                      <FiCopy />{" "}
-                    </button>{" "}
+                      <FiCopy />{' '}
+                    </button>{' '}
                     Copy my email address
                   </li>
                 </ul>
               </div>
-              <div className="contact-list">
-                <Link to="https://github.com/ajeeth-g" target="_blank">
+              <div className='contact-list'>
+                <Link to='https://github.com/ajeeth-g' target='_blank'>
                   Github
                 </Link>
                 <Link
-                  to="https://www.instagram.com/alone_like_men/"
-                  target="_blank"
+                  to='https://www.instagram.com/alone_like_men/'
+                  target='_blank'
                 >
                   Instagram
                 </Link>
                 <Link
-                  to="https://www.facebook.com/profile.php?id=100018785507075"
-                  target="_blank"
+                  to='https://www.facebook.com/profile.php?id=100018785507075'
+                  target='_blank'
                 >
                   facebook
                 </Link>
                 <Link
-                  to="https://www.naukri.com/mnjuser/homepage"
-                  target="_blank"
+                  to='https://www.naukri.com/mnjuser/homepage'
+                  target='_blank'
                 >
                   Naukri
                 </Link>
@@ -142,7 +142,7 @@ const Navbar = ({ mode, setMode }) => {
             </div>
           </div>
         </div>
-        <button onClick={handleDarkMode} className="toggle-mode">
+        <button onClick={handleDarkMode} className='toggle-mode'>
           {mode ? <MdDarkMode /> : <MdOutlineDarkMode />}
         </button>
       </div>
